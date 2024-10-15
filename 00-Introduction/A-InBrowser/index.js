08325f1146b5611415d1aea5c18259c14543ee08
Muhammad Babar Ali <144947128+mbabarali@users.noreply.github.com>
2024-10-15 08:57:12 +0500

    Introduction to JavaScript

---
+++ 00-Introduction/A-InBrowser/index.js
@@ -0,0 +1,15 @@
+/**
+==================================
+ JavaScript in Browser
+==================================
+Requires:
+* Text Editor or IDE
+* JavaScript Runtime Environment
+----------------------------------
+* How to execute the JS program and basic requirements
+  --> VS Code or any other IDE
+  --> Node.js
+==================================
+*/
+
+console.log("This is message in the console of the browser");
---
+++ 00-Introduction/B-InTerminal/index.js
@@ -0,0 +1,15 @@
+/**
+==================================
+ JavaScript in Terminal
+==================================
+Requires:
+* Text Editor or IDE
+* JavaScript Runtime Environment
+----------------------------------
+* How to execute the JS program and basic requirements
+  --> VS Code or any other IDE
+  --> Node.js
+==================================
+*/
+
+console.log("This is message in the terminal");
